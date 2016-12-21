c7d7cc6d8b0315e49b852baba7ca82063d890f54
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2016-12-22 00:18:41 +0100

    Improving style and content

--- featuresList.js
+++ featuresList.js
@@ -22,12 +22,12 @@ app.controller('featuresController', function () {
             description: 'MeshLab offers a series of automatic, semi-manual and interactive filters to remove those geometric element generally considered “wrong” by most software and algorithms. It is possible to removing topological errors, duplicated and unreferenced vertices, small components, degenerated or intersecting faces, and many more geometrical and topological singularities. Using different automatic and interactive selection methods, is then possible to isolate and remove unwanted areas of your meshes and point clouds.'
                             },
         {
-            name: 'Scaling, Positioning and Orienting 3D Models',
+            name: 'Scaling, Positioning and Orienting',
             image: 'img/Feature/Orientation.jpg',
             description: '3D models, especially coming from survey and scanning, often need to be re-oriented, or placed in a specific reference system; additionally, if they have been generated from 3d-from-photos, they generally need scaling to become metric. MeshLab provides a variety of features to manipulate the scale, positioning and orientation of a 3D model, including basic transformation operations like translation/scaling/rotation, automatic re-centering and alignment to axis, geo-referencing with reference points, interactive manipulators for rotation/translation/scaling, and many others.'
                             },
         {
-            name: '3D Models Simplification, Refinement and Remeshing',
+            name: 'Simplification, Refinement and Remeshing',
             image: 'img/Feature/Remeshing.jpg',
             description: 'A common need when processing a 3D model is to reduce its geometric complexity, creating a geometry with the same shape but with less triangles (or points). MeshLab offers different ways to simplify (decimate) triangulated surfaces, able to preserve geometrical detail and texture mapping, or to selectively reduce the number of points in a pointcloud. In other cases, the user may want to increase the number of triangles (or points): MeshLab also provides different subdivision schemes, remeshing and resampling filters to increase geometric complexity of 3D models, or to optimize point distribution and triangulation quality.'
                             },
@@ -44,25 +44,25 @@ app.controller('featuresController', function () {
         {
             name: 'Color Processing',
             image: 'img/Feature/ColorProcessing.jpg',
-            description: 'MeshLab can manipulate the vertex and face colors of the 3D model using a series of photoshop-like filters (gamma, saturation, brightness, contrast, levels, smoothing, sharpening). Automatic filters are available to calculate Ambient Occlusion and Volumetric Obscurance and map it to vertex or face color. It is also possible to explicitly write color function, to highlight specific characteristics of the 3d model. MeshLab also offer a painting interface for vertex colors. Scalar values, possibly the result of a metric calculation on the 3D surface, may also be mapped on vertex/face color, to have a visual representation of that value.'
+            description: 'MeshLab can manipulate the vertex and face colors of the 3D model using a series of photoshop-like filters (gamma, saturation, brightness, contrast, levels, smoothing, sharpening). Automatic filters are available to calculate Ambient Occlusion and Volumetric Obscurance and map it to vertex or face color. It is also possible to explicitly write color function, to highlight specific characteristics of the 3D model. MeshLab also offers a painting interface for vertex colors. Scalar values, possibly the result of a metric calculation on the 3D surface, may also be mapped on vertex/face color, to have a visual representation of that value.'
                             },
         {
-            name: '3D Printing: Offsetting, hollowing, closing, flattening',
+            name: '3D Printing: Offsetting, Hollowing, Closing',
             image: 'img/Feature/HoleFilling.jpg',
             description: 'Beside being able to export to STL (one of the most common formats for 3D printing), MeshLab can be used to prepare 3D models for printing by creating inner shells, resampling/remeshing the 3D model to make slicing easier, closing small holes to obtain watertight meshes, and flattening the bottom area to have a better platform adherence.'
                             },
         {
             name: 'Comparing Models',
             image: 'img/Feature/Comparing.jpg',
-            description: 'Measuring the geometric difference between two 3D models using Hausdorff Distance is a common approach in mesh processing. Many years ago (in 1997!), the Visual Computing Lab developed and freely distributed what become the standard tool for such task, Metro; the related paper has been cited more than one thousand times, thus demonstrating the impact of that work. While the original Metro tool was still a small open source standalone command line program (still available at our web site), MeshLab offers now much more advanced and versatile functionalities for comparing two meshes, that also compute signed distance and may work on point clouds.'
+            description: 'Measuring the geometric difference between two 3D models using Hausdorff Distance is a common approach in mesh processing. Many years ago (in 1997!), the Visual Computing Lab developed and freely distributed what become the standard tool for such task, Metro; the related paper has been cited more than one thousand times. While the original Metro tool was a small open source standalone command line program (still available at our web site), MeshLab offers now much more advanced functionalities for comparing two meshes, that also compute signed distance and may work on point clouds.'
                             },
         {
-            name: '3D Models Conversion and Interchange with other Tools',
+            name: '3D Models Conversion and Interchange',
             image: 'img/Feature/Exporting.jpg',
             description: 'MeshLab can import and export a number of different 3D data formats and to online services like SketchFab. In this way it is possible for the user to interchange data with other tools (including Blender, Photoscan, VisualSfM, Cloud Compare, Autodesk tools), working in the context of complex 3D processing pipelines in a number of different contexts and applications. The scripting functionalities will make this type of use even easier and unattended.'
                             },
         {
-            name: 'Raster Layers: integration with images',
+            name: 'Raster Layers: Integration with Images',
             image: 'img/Feature/Raster.jpg',
             description: 'Raster Layers have been introduced in MeshLab to allow to go beyond the standard 3D model. MeshLab users can import in a project also images and other 2D entities. These Raster Layers can be used not only to project color information on the 3D model, but also to generate peculiar points of view, or store an entire 3d-from-image acquisition procedure by including also the images used for the generation of the final results.'
                             }
